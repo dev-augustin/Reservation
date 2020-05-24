88316ce967f628697e13b570528e03cce6e47e3f
dev-augustin <dev.augustin00@gmail.com>
2020-05-24 16:56:21 -0500

    added initial routes

--- room-reservation/src/App.js
+++ room-reservation/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render(){
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/rooms' exact={true} component={Rooms}/>
+          <Route path='/amenities' exact={true} component={Amenities}/>
+          <Route path='/makeReservation' exact={true} component={MakeReservation}/>
+          <Route path='/guestReview' exact={true} component={GuestReview}/>
+        </Switch>
+
+      </Router>
+    );
+  }
 }
 
 export default App;
