dafb3d78ffe394b6e0f905114cc6ad7138ecda3a
dev-augustin <dev.augustin00@gmail.com>
2020-05-25 17:35:07 -0500

    added things to do route link

--- room-reservation/src/components/Navigation.js
+++ room-reservation/src/components/Navigation.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import LinkContainer from 'react-router-bootstrap'
+import '../styles/Navigation.css'
 
 export default function Navigation() {
     return (
-        <div>   
-           <ul>
+        <div className="route-container">   
+           <ul className="route-navBar">
                <li><Link to='/'>Home</Link></li>
                <li><Link to='/makeReservation'>Make Reservation</Link></li>
                <li><Link to='/amenities'>Amenities</Link></li>
                <li><Link to='/rooms'>Rooms</Link></li>
                <li><Link to='/reviews'>Guest Review</Link></li>
                <li><Link to='/manageReservation'>Manage Reservation</Link></li> 
+               <li><Link to='/thingsToDo'>Explore</Link></li> 
            </ul> 
         </div>
     )
