a4e019b945ff6c42e61ba6819bea067b34fa42c8
dev-augustin <61803266+dev-augustin@users.noreply.github.com>
2020-05-28 17:24:41 -0500

    code clean up

--- room-reservation/src/components/Navigation.js
+++ room-reservation/src/components/Navigation.js
@@ -8,14 +8,6 @@ import {Navbar, Nav} from 'react-bootstrap'
 export default function Navigation() {
     return (
         <div className="route-container">   
-         {/* <div>
-            <h1 id="hotel-name">
-                <RainbowText lightness={0.5} saturation={1}> Rainbow Inn</RainbowText></h1>
-         </div> */}
-           {/* <Navbar bg="light">
-    <Navbar.Brand href="#home">Brand Nav.Link</Navbar.Brand>
-  </Navbar>
-  <br /> */}
   <Navbar variant="light" className="nav-color" >
     <Navbar.Brand>
         <h1 id="hotel-name">
@@ -32,17 +24,7 @@ export default function Navigation() {
       <Nav.Link href='/about'>About Us | Contact</Nav.Link>
       </Nav>
   </Navbar>
-           {/* <div>
-               <ul className="route-navBar">
-                    <li><Nav.Link href='/'>Home</Nav.Link></li>
-                    <li><Nav.Link href='/rooms'>Rooms</Nav.Link></li>
-                    <li><Nav.Link href='/amenities'>Amenities</Nav.Link></li>
-                    <li><Nav.Link href='/makeReservation'>Reserve Room</Nav.Link></li>
-                    <li><Nav.Link href='/manageReservation'>Manage Reservation</Nav.Link></li> 
-                    <li><Nav.Link href='/reviews'>Guest Review</Nav.Link></li>
-                    <li><Nav.Link href='/thingshrefDo'>Explore</Nav.Link></li> 
-                </ul> 
-           </div> */}
+       
         </div>
     )
 }
